7e6b8b099f5b3006efb16488bad4a8b273c89129
Ognjen7 <105551063+Ognjen7@users.noreply.github.com>
2024-08-22 17:09:57 +0200

    implemented libraries that allow a user to generate a pdf report of expenses and incomes and send then via email

---
+++ ExpenseTracker/ExpenseTracker/Services/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using ExpenseTracker.Services.Interfaces;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace ExpenseTracker.Services;
+
+public class EmailService : IEmailService
+{
+    IConfiguration _configuration;
+
+    public EmailService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string message, byte[]? attachment, string? attachmentName)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress("Expense Tracker", _configuration["Email:From"]));
+        emailMessage.To.Add(new MailboxAddress("", email));
+        emailMessage.Subject = subject;
+
+        var bodyBuilder = new BodyBuilder { HtmlBody = message };
+
+        if (attachment != null && attachment.Length > 0)
+        {
+            bodyBuilder.Attachments.Add(attachmentName, attachment);
+        }
+
+        emailMessage.Body = bodyBuilder.ToMessageBody();
+
+        using (var client = new SmtpClient())
+        {
+            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+            await client.ConnectAsync(_configuration["Email:SmtpServer"], int.Parse(_configuration["Email:Port"]), bool.Parse(_configuration["Email:UseSsl"]));
+            await client.AuthenticateAsync(_configuration["Email:Username"], _configuration["Email:Password"]);
+
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace ExpenseTracker.Services.Interfaces;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string email, string subject, string message, byte[]? attachment, string? attachmentName);
+}
